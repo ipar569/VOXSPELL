952713125ee5535ee676046799ed9bdfaada401e
ipar569 <ipar569@aucklanduni.ac.nz>
2016-09-13 20:16:46 +1200

    WordList class from Injae Park is edited to meet the requirement. (by Injae)
    Changes:
    - The constructor initialise the array list of integers containing the position of where a specific level starts.
    - getRandomCount() method now takes argument (int level) and it is used to get the word count of specific level.
    - getRamdomWord() method now takes argument (int level) and returns random number from the level.
    Added:
    - createTestList() method is added. This method takes integer argument and builds a list of 10 or less words ( if there is less than 10 words) for a specific level defined by the argument.

---
+++ WordList.java
@@ -0,0 +1,68 @@
+package prototype;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Random;
+
+
+//Written by Injae Park
+public class WordList {
+	
+	private ArrayList<String> _wordList=new ArrayList<String>();
+	private int[] pos = new int[11];
+	
+	public WordList(String file) throws IOException{
+		
+			BufferedReader wordlist = new BufferedReader(new FileReader("."+File.separator+file));
+			
+			String line;
+			int a=0;
+			//Reading word where and adding it to arrayList if it is not an empty line
+			while ((line = wordlist.readLine()) != null){
+				if(!"".equals(line.trim())){
+					_wordList.add(line);
+					String[] lines = line.split("");
+					if(lines[0].equals("#")){
+						pos[a] = _wordList.size()-1;
+						a++;
+					}
+				}
+			}
+	}
+	
+	//Return wordCount for each level.
+	public int getWordCount(int level){
+		return pos[level]-pos[level-1];
+	}
+	
+	//Get random word from the arrayList
+	public String getRandomWord(int level){
+		Random r = new Random();
+		
+		//Getting random position
+		int rand = Math.abs(r.nextInt()) % this.getWordCount(1);
+		
+		return _wordList.get(rand+pos[level-1]);
+	}
+	
+	//Sort the list and return the word at the position specified.
+	public String getSortedWord(int a){
+		Collections.sort(_wordList);
+		return _wordList.get(a);
+	}
+	
+	public ArrayList<String> createTestList(int level){
+		LinkedHashSet<String> list = new LinkedHashSet<String>();
+		while(list.size()<10||list.size()==getWordCount(level))
+			list.add(getRandomWord(level));
+		
+		ArrayList<String> testList = new ArrayList<String>();
+		testList.addAll(list);
+		return testList;
+	}
+}
